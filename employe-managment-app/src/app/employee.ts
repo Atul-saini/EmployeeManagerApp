4e462b2871fc0b1d3ca1b8dcfa6cf3d23232d7a9
Atul Saini <atul.saini@peoplestrong.com>
2022-08-02 22:21:56 +0530

    first commit

---
+++ employe-managment-app/src/app/employee.ts
@@ -0,0 +1,12 @@
+import { NumberValueAccessor } from "@angular/forms";
+
+export interface Employee {
+    id: number;
+    name:String;
+    email : String;
+    jobTitle: String;
+    phone:String;
+    imageUrl:String;
+    employeeCode:String;
+    
+}
